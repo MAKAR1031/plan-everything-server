00a38f6657505cbc43aaa8f1792e568652d2cdca
Sergey Makarov <makar1031@yandex.ru>
2018-03-19 20:08:15 +0300

    авторизация через Facebook

---
+++ src/main/java/ru/migmak/planeverything/server/config/SecurityConfiguration.java
@@ -0,0 +1,33 @@
+package ru.migmak.planeverything.server.config;
+
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+@Profile("!dev")
+@Configuration
+@EnableOAuth2Sso
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.antMatcher("/**")
+            .authorizeRequests()
+            .antMatchers("/login")
+            .permitAll()
+            .anyRequest()
+            .authenticated()
+            .and()
+            .logout()
+            .logoutSuccessUrl("/login")
+            .permitAll()
+            .deleteCookies()
+            .and()
+            .csrf()
+            .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+    }
+}
