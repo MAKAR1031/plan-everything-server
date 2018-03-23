a8985d9337761d0141daf899d4744cfbd1c6c194
Sergey Makarov <makar1031@yandex.ru>
2018-03-23 20:40:42 +0300

    получение всех проектов текущего пользователя

--- src/main/java/ru/migmak/planeverything/server/security/config/SecurityConfiguration.java
+++ src/main/java/ru/migmak/planeverything/server/security/config/SecurityConfiguration.java
@@ -1,11 +1,13 @@
 package ru.migmak.planeverything.server.security.config;
 
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.data.repository.query.SecurityEvaluationContextExtension;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Profile("!dev")
@@ -29,5 +31,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .and()
             .csrf()
             .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+    }
+
+    @Bean
+    public SecurityEvaluationContextExtension securityEvaluationContextExtension() {
+        return new SecurityEvaluationContextExtension();
     }
 }
