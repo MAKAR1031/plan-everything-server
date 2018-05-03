20992ba74f06a00bbe1e628872c36882c5b0318f
Sergey Makarov <makar1031@yandex.ru>
2018-05-04 02:14:46 +0300

    изменена структура RO объекта регистрации

--- src/main/java/ru/migmak/planeverything/server/ro/AccountRegistration.java
+++ src/main/java/ru/migmak/planeverything/server/ro/AccountRegistration.java
@@ -7,17 +7,18 @@ import ru.migmak.planeverything.server.domain.Account;
 @Getter
 @Setter
 public class AccountRegistration {
+    private String firstName;
+    private String lastName;
     private String login;
     private String password;
     private String email;
-    private String fullName;
 
     public Account toAccount() {
         Account account = new Account();
+        account.setFullName(lastName.trim() + " " + firstName.trim());
         account.setLogin(login);
         account.setPassword(password);
         account.setEmail(email);
-        account.setFullName(fullName);
         return account;
     }
 }
