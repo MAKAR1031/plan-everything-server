2cdd29eec9ef6e249467a59d067a82a734305308
Sergey Makarov <makar1031@yandex.ru>
2018-05-08 20:07:25 +0300

    убрано исключение поля код из объекта роли участника

--- src/main/java/ru/migmak/planeverything/server/domain/MemberRole.java
+++ src/main/java/ru/migmak/planeverything/server/domain/MemberRole.java
@@ -1,6 +1,5 @@
 package ru.migmak.planeverything.server.domain;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -19,7 +18,6 @@ public class MemberRole {
     @Column(nullable = false, unique = true, length = 80)
     private String name;
 
-    @JsonIgnore
     @Column(nullable = false, unique = true, length = 20)
     private String code;
 
