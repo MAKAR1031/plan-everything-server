6502132fd3bc35ef03a74396a8dc0c78ce40c60a
Sergey Makarov <makar1031@yandex.ru>
2018-05-08 21:16:43 +0300

    к ресурсу проекта добавлена ссылка о возможности управлять задачами

--- src/main/java/ru/migmak/planeverything/server/resource/processor/ProjectResourceProcessor.java
+++ src/main/java/ru/migmak/planeverything/server/resource/processor/ProjectResourceProcessor.java
@@ -19,6 +19,7 @@ import ru.migmak.planeverything.server.service.AccountService;
 @RequiredArgsConstructor
 public class ProjectResourceProcessor implements ResourceProcessor<Resource<Project>> {
 
+    private static final String MANAGE_TASKS = "manageTasks";
     private static final String MANAGE_TAGS = "manageTags";
     private static final String MANAGE_MEMBERS = "manageMembers";
     private static final String MANAGE_HREF = "manage";
@@ -26,25 +27,28 @@ public class ProjectResourceProcessor implements ResourceProcessor<Resource<Proj
     private final RepositoryEntityLinks links;
 
     @Override
-    public Resource<Project> process(Resource<Project> projectResource) {
-        final Project project = projectResource.getContent();
+    public Resource<Project> process(Resource<Project> resource) {
+        final Project project = resource.getContent();
         Account currentAccount = accountService.getCurrentAccount();
         project.getMembers().stream()
                 .filter(m -> m.getAccount().getId().equals(currentAccount.getId()))
                 .findFirst()
-                .ifPresent(m -> projectResource.add(createLinkToMember(m)));
+                .ifPresent(m -> resource.add(createLinkToMember(m)));
         ProjectMember currentMember = project.getMembers()
                 .stream()
                 .filter(member -> member.getAccount().getId().equals(currentAccount.getId()))
                 .findAny()
                 .orElseThrow(() -> new BadRequestException("User is not a member of the project"));
         if (currentMember.hasPrivilege(PrivilegeCode.MANAGE_TAGS)) {
-            projectResource.add(new Link(MANAGE_HREF, MANAGE_TAGS));
+            resource.add(new Link(MANAGE_HREF, MANAGE_TAGS));
         }
         if (currentMember.hasPrivilege(PrivilegeCode.MANAGE_MEMBERS)) {
-            projectResource.add(new Link(MANAGE_HREF, MANAGE_MEMBERS));
+            resource.add(new Link(MANAGE_HREF, MANAGE_MEMBERS));
         }
-        return projectResource;
+        if (currentMember.hasPrivilege(PrivilegeCode.MANAGE_TASKS)) {
+            resource.add(new Link(MANAGE_HREF, MANAGE_TASKS));
+        }
+        return resource;
     }
 
     private Link createLinkToMember(ProjectMember member) {
