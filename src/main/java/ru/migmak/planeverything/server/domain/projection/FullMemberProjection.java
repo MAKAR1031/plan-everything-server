2ef2869aa2a0275df50cb2d9893b71bf8f04ef81
Sergey Makarov <makar1031@yandex.ru>
2018-05-08 21:14:30 +0300

    в проекцию участника проекта добавлено более подробное представление роли

--- src/main/java/ru/migmak/planeverything/server/domain/projection/FullMemberProjection.java
+++ src/main/java/ru/migmak/planeverything/server/domain/projection/FullMemberProjection.java
@@ -1,17 +1,17 @@
 package ru.migmak.planeverything.server.domain.projection;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.rest.core.config.Projection;
 import ru.migmak.planeverything.server.domain.Account;
+import ru.migmak.planeverything.server.domain.MemberRole;
 import ru.migmak.planeverything.server.domain.ProjectMember;
 
 @Projection(name = "full", types = ProjectMember.class)
+@SuppressWarnings("unused")
 public interface FullMemberProjection {
 
     Long getId();
 
     Account getAccount();
 
-    @Value("#{target.role.name}")
-    String getRole();
+    MemberRole getRole();
 }
